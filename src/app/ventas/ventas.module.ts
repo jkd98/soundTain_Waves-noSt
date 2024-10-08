64c80911aced50828fad9a957b1c9cb20197b6ec
Jesus Padron <jesusyaelpadrongrimaldo@gmail.com>
2024-10-07 19:54:45 -0600

    Se agregar el carrito componente

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarritoComponent
+  ],
   imports: [
     CommonModule
   ]
