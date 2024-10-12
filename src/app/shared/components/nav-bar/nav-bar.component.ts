41913765a0000b3ed3cc34008c10ff4632d5c732
jk89 <davidlrj9999@gmail.com>
2024-10-12 10:41:01 -0600

    Ajuste a header y Nav

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,6 +12,19 @@ export class NavBarComponent {
 
   onShowMenu():void {
     this.showMenu = !this.showMenu;
+  }
+
+
+  // Método que se ejecuta al hacer clic fuera del menú
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    const target = event.target as HTMLElement;
+    const isClickInsideMenu = target.closest('.nav__links') || target.closest('.hamburger__btn');
+
+    // Si el clic no está dentro del menú o botón, oculta el menú
+    if (!isClickInsideMenu && this.showMenu) {
+      this.showMenu = false;
+    }
   }
   
 }
