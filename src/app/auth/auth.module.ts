a80104d0676a426daf0aee91467591f72fc34d0d
Jesus Padron <jesusyaelpadrongrimaldo@gmail.com>
2024-10-14 20:12:21 -0600

    Cambios inicio de sesion

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
+import { AuthRoutingModule } from './auth-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPagesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
