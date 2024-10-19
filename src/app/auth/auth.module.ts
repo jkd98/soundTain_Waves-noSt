1183bfbc5517cc2c83021a5cd9d175bfceb15312
jkd98 <165828084+jkd98@users.noreply.github.com>
2024-10-18 19:02:42 -0600

    Revert "añadiendo modulo admin"

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,21 +1,12 @@
-import { AuthRoutingModule } from './auth-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
-  declarations: [
-    LoginPagesComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    AuthRoutingModule,
-    SharedModule,
-    ReactiveFormsModule,
-
+    CommonModule
   ]
 })
 export class AuthModule { }
--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LayoutcltPageComponent } from "./pages/layoutclt-page/layoutclt-page.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { InstrumentosGeneralComponent } from './pages/instrumentos-general/instrumentos-general.component';
 
 const routes: Routes = [
     {
